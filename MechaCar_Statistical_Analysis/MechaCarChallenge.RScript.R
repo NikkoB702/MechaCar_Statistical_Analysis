aad8c70a9ff3c6163a15f9cf7c8d785c37d730de
NikkoB702 <80132877+NikkoB702@users.noreply.github.com>
2022-12-23 12:27:22 -0800

    3rd upload

--- MechaCar_Statistical_Analysis/MechaCarChallenge.RScript.R
+++ MechaCar_Statistical_Analysis/MechaCarChallenge.RScript.R
@@ -7,3 +7,15 @@ head(my_csv)
 lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length, data=my_csv)
 
 summary(lm(mpg ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + vehicle_length, data=my_csv))
+
+sus_csv <- read.csv("Suspension_Coil.csv", stringsAsFactors = F)
+
+head(sus_csv)
+
+total_summary <- sus_csv %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+head(total_summary)
+
+lot_summary <- sus_csv %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+head(lot_summary)
