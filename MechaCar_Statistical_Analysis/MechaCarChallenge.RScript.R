4a9a3ee2b3172d8c403bd9c6f75eea629c5f3334
NikkoB702 <80132877+NikkoB702@users.noreply.github.com>
2022-12-23 12:36:32 -0800

    3rd upload

--- MechaCar_Statistical_Analysis/MechaCarChallenge.RScript.R
+++ MechaCar_Statistical_Analysis/MechaCarChallenge.RScript.R
@@ -19,3 +19,19 @@ head(total_summary)
 lot_summary <- sus_csv %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
 head(lot_summary)
+
+sample_data <- sus_csv %>% sample_n(50)
+
+t.test((sample_data$PSI), mu=1500)
+
+sample_1 <- sus_csv %>% subset(Manufacturing_Lot=="Lot1") %>% sample_n(25)
+
+sample_2 <- sus_csv %>% subset(Manufacturing_Lot=="Lot2") %>% sample_n(25)
+
+sample_3 <- sus_csv %>% subset(Manufacturing_Lot=="Lot3") %>% sample_n(25)
+
+t.test((sample_1$PSI), mu=1500)
+
+t.test((sample_2$PSI), mu=1500)
+
+t.test((sample_3$PSI), mu=1500)
